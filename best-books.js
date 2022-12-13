5f914384a5b27b01bc268e8165c518fe75c02761
Nandini Rao [nandinirao] <nandinirao@costco.com>
2022-12-12 21:18:54 -0800

    hw 8

--- best-books.js
+++ best-books.js
@@ -1,17 +1,45 @@
-const formEl = document.getElementById('best-books-form');
-const yearEl = document.getElementById('year');
-const monthEl = document.getElementById('month');
-const dateEl = document.getElementById('date');
+const formEl = document.querySelector('#best-books-form');
+const yearEl = document.querySelector('#year');
+const monthEl = document.querySelector('#month');
+const dateEl = document.querySelector('#date');
 
-formEl.addEventListener('submit', function(e) {
+const displayResults = (bookList) => {
+  const displayDiv = document.getElementById('books-container');
+  displayDiv.innerHTML = "<strong>Top 5 Hardcover Bestsellers</strong>";
+
+  let top5 = bookList.results.books.filter(book => book.rank <= 5);
+  top5.forEach(book => {
+    //console.log(book);
+
+    let liElement = document.createElement('li');
+    const spanElement = document.createElement('span');
+    spanElement.innerHTML = `<strong>${book.title} by ${book.author} </strong>
+    <p>
+    <img src="${book.book_image}">
+    <p>${book.description}<p>`;
+
+    liElement.appendChild(spanElement);
+    displayDiv.appendChild(liElement);
+  });
+
+};
+
+formEl.addEventListener('submit', function (e) {
   e.preventDefault();
 
   const year = yearEl.value;
   const month = monthEl.value;
   const date = dateEl.value;
 
-  // Fetch bestselling books for date and add top 5 to page
-  //api key = 
+  const url = `https://api.nytimes.com/svc/books/v3/lists/${year}-${month}-${date}/hardcover-fiction.json?api-key=${apiKey}`;
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((json) => displayResults(json))
+    .catch((err) => console.error(`Fetch problem: ${err.message}`));
 
-  //https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=
 });
